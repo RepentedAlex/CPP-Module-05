39ba1db01c4ba018ea2ec439d1ca9ea24df7e515
apetitco <apetitco@student.42.fr>
2025-06-26 15:39:51 +0200

    ex00 should be ok

---
+++ ex00/src/Bureaucrat.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+
+#include "Bureaucrat.hpp"
+
+///CONSTRUCTORS/////////////////////////////////////////////////////////////////
+Bureaucrat::Bureaucrat(std::string name, unsigned int grade) : _name(name), _grade(grade)
+{
+	if (grade < 1)
+		throw GradeTooHighException();
+	if (grade > 150)
+		throw GradeTooLowException();
+}
+
+Bureaucrat::Bureaucrat(unsigned int grade) : _name("Scapegoat"), _grade(grade)
+{
+	if (grade < 1)
+		throw GradeTooHighException();
+	if (grade > 150)
+		throw GradeTooLowException();
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &original)
+{
+	*this = original;
+}
+
+///DESTRUCTORS//////////////////////////////////////////////////////////////////
+Bureaucrat::~Bureaucrat()
+{
+
+}
+
+///OPERATOR OVERLOADS///////////////////////////////////////////////////////////
+Bureaucrat	&Bureaucrat::operator=(const Bureaucrat &original)
+{
+	if (this != &original)
+	{
+		this->_grade = original.getGrade();
+	}
+	return (*this);
+}
+
+///GETTERS//////////////////////////////////////////////////////////////////////
+unsigned int	Bureaucrat::getGrade() const
+{
+	return (this->_grade);
+}
+
+const std::string	Bureaucrat::getName() const
+{
+	return (this->_name);
+}
+
+///SETTERS//////////////////////////////////////////////////////////////////////
+void	Bureaucrat::setGrade(const int grade)
+{
+	if (grade < 1)
+		throw GradeTooHighException();
+	if (grade > 150)
+		throw GradeTooLowException();
+	else
+		this->_grade = grade;
+}
+
+
+///MEMBER FUNCTIONS/////////////////////////////////////////////////////////////
+void	Bureaucrat::decrementGrade()
+{
+	if (this->getGrade() >= 150)
+		throw GradeTooLowException();
+	this->_grade++;
+}
+
+void	Bureaucrat::incrementGrade()
+{
+	if (this->getGrade() <= 1)
+		throw GradeTooHighException();
+	this->_grade--;
+}
+
+///EXCEPTIONS///////////////////////////////////////////////////////////////////
+const char	*Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return ("Error: Grade too high.");
+}
+
+const char	*Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return ("Error: Grade too low.");
+}
+
+
+
+///FOREIGN OVERLOADS////////////////////////////////////////////////////////////
+std::ostream	&operator<<(std::ostream &o, Bureaucrat &bureaucrat)
+{
+	o << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade() << '.' << std::endl;
+	return (o);
+}
---
+++ ex00/src/main.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+
+#include "Bureaucrat.hpp"
+
+#define BLUE	"\033[1;95m"
+#define CYAN	"\033[36m"
+#define YELLOW	"\033[4;33m"
+#define RESET	"\033[0m"
+
+#define PRINT_SECTION(x) \
+do { \
+	std::cout << BLUE "=== " x " ===" RESET << std::endl; \
+} while (0)
+
+
+#define PRINT_SUBSECTION(x) \
+do { \
+	std::cout << YELLOW "~ " x " ~" RESET << std::endl; \
+} while (0)
+
+#define PRINT_TEST(x) \
+do { \
+	std::cout << CYAN x RESET << std::endl; \
+} while (0)
+
+#define PRINT(x) do { std::cout << x << std::endl; } while (0)
+
+int	main()
+{
+	{
+		PRINT_SECTION("TESTING VALID");
+		Bureaucrat	regis("Régis", 42);
+
+		std::cout << regis;
+
+		PRINT_SUBSECTION("Incrementing grade");
+		regis.incrementGrade();
+		std::cout << regis;
+
+		PRINT_SUBSECTION("Decrementing grade");
+		regis.decrementGrade();
+		std::cout << regis;
+
+	}
+
+	std::cout << std::endl;
+
+	{
+		PRINT_SECTION("TESTING EXCEPTIONS");
+
+		PRINT_SUBSECTION("Constructing with invalid grades");
+		PRINT_TEST("Grade == 0 (Too high)");
+		try
+		{
+			Bureaucrat	timeo("Timéo", 0);
+		}
+		catch (Bureaucrat::GradeTooHighException &e)
+		{
+			std::cout << "Couldn't create Timéo: " << e.what() << std::endl;
+		}
+
+		PRINT_TEST("Grade == 151 (Too low)");
+		try
+		{
+			Bureaucrat	cunegonde("Cunégonde", 151);
+		}
+		catch (Bureaucrat::GradeTooLowException &e)
+		{
+			std::cout << "Couldn't create Timéo: " << e.what() << std::endl;
+		}
+
+		std::cout << std::endl;
+
+		PRINT_SUBSECTION("Incrementing/Decrementing outside of acceptable range");
+		Bureaucrat	melissandre("Mélissandre", 150);
+		Bureaucrat	celestin("Célestin", 1);
+
+		PRINT_TEST("Decrementing lowest grade: ");
+		try
+		{
+			melissandre.decrementGrade();
+		}
+		catch (Bureaucrat::GradeTooLowException &e)
+		{
+			std::cout << melissandre << e.what() << std::endl;
+		}
+		PRINT_TEST("Incrementing highest grade: ");
+		try
+		{
+			celestin.incrementGrade();
+		}
+		catch (Bureaucrat::GradeTooHighException &e)
+		{
+			std::cout << celestin << e.what() << std::endl;
+		}
+	}
+
+	return (0);
+}
